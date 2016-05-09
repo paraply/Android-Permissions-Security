250d365c4458dc63061aa0d9f95b384e3d95af7c
Mikael <git@evinja.se>
2016-05-09 09:58:50 +0200

    Unique ID

--- app/src/main/java/tan/philip/androidpermissionssecurity/MainActivity.java
+++ app/src/main/java/tan/philip/androidpermissionssecurity/MainActivity.java
@@ -2,6 +2,7 @@ package tan.philip.androidpermissionssecurity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String uid = new UniqueID().getID(getApplicationContext());
+        Log.i("GOT ID", uid);
+
     }
 }
